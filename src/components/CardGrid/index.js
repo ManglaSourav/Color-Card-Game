cf5424b8487e87f934cf1db84e1b73d935fd06cb
Sourav Mangla <mangla.sourav96@gmail.com>
2019-11-10 22:19:13 +0530

    minor changes

--- src/components/CardGrid/index.js
+++ src/components/CardGrid/index.js
@@ -1,30 +1,24 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import ColorCard from "components/ColorCard";
 import styled from "styled-components";
 import { device } from "utils/device";
 import { getColors } from "utils/colorsUtils";
 
 export default function CardGrid() {
-  const difficulty = 3;
+  const difficulty = 4;
   const colors = useRef(getColors(difficulty) || []);
   const [clickedCards, setClickedCard] = useState([]);
 
-  useEffect(() => {
-    console.log(clickedCards);
-  }, [clickedCards]);
-  console.log(colors);
-
   return (
-    <GridWrapper rows={`repeat(${difficulty},1fr)`}>
+    <GridWrapper rows={`repeat(${difficulty > 4 ? 4 : difficulty},1fr)`}>
       {colors.current.length > 0 &&
         colors.current.map((color, i) => (
           <ColorCard
-            visibility={true}
-            shouldUpdate={
-              clickedCards.length <= colors.current.length ? true : false
-            }
+            shouldUpdate={clickedCards.length < colors.current.length}
             setClickedCard={setClickedCard}
+            clickedCards={clickedCards}
             key={i}
+            index={i}
             color={color}></ColorCard>
         ))}
     </GridWrapper>
@@ -39,9 +33,6 @@ const GridWrapper = styled.div`
   justify-content: center;
   margin-top: 10%;
   @media ${device.mobileL} {
-    /* display: block; */
-    /* display: grid; */
-    /* grid-template-columns: repeat(3, 1fr); */
     grid-gap: 5px 5px;
   }
 `;
--- src/components/ColorCard/index.js
+++ src/components/ColorCard/index.js
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { device } from "utils/device";
 
+//checek clickedCards array contain even number of colors and if not it should contain the incoming color than it should push into the array
 export default function ColorCard(props) {
-  const { color, setClickedCard, visibility, shouldUpdate } = props;
+  const {
+    color,
+    setClickedCard,
+    visibility,
+    shouldUpdate,
+    index,
+    clickedCards
+  } = props;
   const [clicked, setClicked] = useState(false);
-  const onCardClicked = () => {
+  const onCardClicked = e => {
+    console.log(e.target.value);
+
     setClicked(true);
+    // console.log(clickedCards.filter(card => card.value === e.target.value));
     if (shouldUpdate) {
+      // if(clickedCards.includes(color)){
+      // }
       setClickedCard(prev => [...prev, color]);
     }
   };
@@ -14,8 +28,16 @@ export default function ColorCard(props) {
   return (
     <>
       <Card
-        visiblity={visibility ? true : false}
+        // visibility={visibility ? "visible" : "hidden"}
+        visibility={
+          clickedCards.filter(clickedColor => color === clickedColor).length ===
+          2
+            ? false
+            : true
+        }
         color={color}
+        value={index}
+        flip={false}
         clicked={clicked ? true : ""}
         onClick={onCardClicked}></Card>
     </>
@@ -23,18 +45,21 @@ export default function ColorCard(props) {
 }
 
 const Card = styled.div`
-  visibility: ${props => (props.visibility ? "hidden" : "")};
-  height: 100px;
-  width: 100px;
+  visibility: ${props => (props.visibility ? "visible" : "hidden")};
+  height: 200px;
+  width: 200px;
   background: ${props => (props.clicked ? props.color : "tomato")};
   margin: 10px;
   align-self: center;
   justify-self: center;
   border-radius: 20px;
   box-shadow: 1px 1px 1px;
-  transition: all 0.2s;
+  transition: all 0.8s;
   &:active {
-    transform: rotate(180deg);
-    /* background: red; */
+    transform: ${props => props.flip && `rotateY(180deg)`};
+  }
+  @media ${device.mobileL} {
+    height: 100px;
+    width: 100px;
   }
 `;
