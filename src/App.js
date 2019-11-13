0baf9545eabc065e6a37ace8fc1895d298a29e6d
Sourav Mangla <mangla.sourav96@gmail.com>
2019-11-13 13:44:23 +0530

    fix:UI"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import CardGrid from "components/CardGrid";
+import Header from "components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <CardGrid></CardGrid>
     </div>
   );
